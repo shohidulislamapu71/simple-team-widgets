bea86b86956fb5038bedc6dfa161813e69013ddd
Shohidul Islam Apu <118607648+shohidulislamapu71@users.noreply.github.com>
2024-08-21 17:33:54 +0600

    ready to work

--- widgets/team-widgets.php
+++ widgets/team-widgets.php
@@ -83,9 +83,49 @@ class Elementor_Team_Widget extends \Elementor\Widget_Base {
 		   
 		    ]
 	    );
-		
-		
-
+		// Team style layout
+		$this->add_control(
+			'simple_team_layout',
+			[
+				'label' => esc_html__( 'Simple Team Style', 'simple-team-widgets' ),
+				'type' => \Elementor\Controls_Manager::SELECT,
+				'simple-team-style-01' => esc_html__('Simple Team Style 01','simple-team-widgets'),
+				'options' => [
+					'simple-team-style-01' => esc_html__( 'Team Style 01', 'simple-team-widgets' ),
+					'simple-team-style-02' => esc_html__( 'Team Style 02', 'simple-team-widgets' ),
+				],
+			]
+		);
+		// Team list info
+		$this->add_control(
+			'list',
+			[
+				'label' => esc_html__( 'Repeater List', 'simple-team-widgets' ),
+				'type' => \Elementor\Controls_Manager::REPEATER,
+				'fields' => [
+					[
+						'name' => 'list_title',
+						'label' => esc_html__( 'Title', 'simple-team-widgets' ),
+						'type' => \Elementor\Controls_Manager::TEXT,
+						'default' => esc_html__( 'List Title' , 'simple-team-widgets' ),
+						'label_block' => true,
+					],
+					
+					
+				],
+				'default' => [
+					[
+						'list_title' => esc_html__( 'Title #1', 'simple-team-widgets' ),
+						'list_content' => esc_html__( 'Item content. Click the edit button to change this text.', 'simple-team-widgets' ),
+					],
+					[
+						'list_title' => esc_html__( 'Title #2', 'simple-team-widgets' ),
+						'list_content' => esc_html__( 'Item content. Click the edit button to change this text.', 'simple-team-widgets' ),
+					],
+				],
+				'title_field' => '{{{ list_title }}}',
+			]
+		);
 		$this->end_controls_section();
 		// end of the Style tab section
 
@@ -102,10 +142,20 @@ class Elementor_Team_Widget extends \Elementor\Widget_Base {
 	protected function render() {
 		// get our input from the widget settings.
 		$settings = $this->get_settings_for_display();
-	
-		?>
-		
-	
-       <?php
+		$team_layout = $settings['simple_team_layout'];
+		switch ($team_layout) {
+			case 'simple-team-style-01':
+				include(__DIR__.'/includes/simple-team-style-01.php');
+				break;
+
+			case 'simple-team-style-02':
+				include(__DIR__. '/includes/simple-team-style-02.php');
+				break;
+
+			default:
+				include(__DIR__. '/includes/simple-team-style-01.php');
+				break;
+		}
+
 	}
 }
